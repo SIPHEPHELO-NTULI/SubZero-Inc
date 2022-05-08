7a825e721cb6484cc2a168cc41fb0ffe88208c3b
SIPHEPHELO-NTULI <101139347+SIPHEPHELO-NTULI@users.noreply.github.com>
2022-05-08 18:52:09 +0200

    Create email_password_validator_test.dart

---
+++ email_password_validator_test.dart
@@ -0,0 +1,26 @@
+import 'package:swap_shop/screens/login_screen.dart';
+import 'package:test/test.dart'; //packages with nodes
+
+void main(){
+
+  test('empty email returns error string',() async{
+    var result = EmailFieldValidator.validate('');
+    expect(result, "Please Enter Your Email");
+  });
+
+  test('non-empty email returns null',()async{
+    var result = EmailFieldValidator.validate('email');
+    expect(result, "Please enter a valid email");
+  });
+
+  test('empty password returns error string',()async{
+    var result = PasswordFieldValidator.validate('');
+    expect(result, "Password Required");
+  });
+
+  test('non-empty password returns null',()async{
+    var result = PasswordFieldValidator.validate('pass');
+    expect(result, "Please Enter Valid Password (6 Characters Min)");
+  });
+
+}
