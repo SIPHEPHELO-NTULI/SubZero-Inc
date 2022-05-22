322e94fe8c0cbda020887aadc09f2999989bdda4
SIPHEPHELO-NTULI <101139347+SIPHEPHELO-NTULI@users.noreply.github.com>
2022-05-22 23:18:05 +0200

    Update registration_screen_test.dart

--- test/screens/registration_screen_test.dart
+++ test/screens/registration_screen_test.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:swap_shop/main.dart';
+import 'package:swap_shop/screens/login_screen.dart';
 import 'package:swap_shop/screens/registration_screen.dart';
 
 
+
+
 void main() {
+  
   test('name empty', () async {
     var result = nameFieldValidator.validate('');
     expect(result, "Name Cannot be Empty");
@@ -32,5 +40,28 @@ void main() {
     var result = confirmPasswordValidator.validate('');
     expect(result,"Passwords don't match");
   });
-  
+
+  //Widget createLoginScreen() => Ch
+
+ group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
+  });
 }
